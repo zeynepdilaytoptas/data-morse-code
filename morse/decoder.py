aec2cb56853232dadee3fab5b95e981c643ba1ee
zeynepdilaytoptas <zezidel@gmail.com>
2025-12-16 16:24:50 +0100

    Finished the decoder

--- morse/decoder.py
+++ morse/decoder.py
@@ -1,9 +1,30 @@
-pass  # YOUR CODE HERE
+from morse.mapping import MORSE
+
+REVERSE_MORSE = {value: key for key, value in MORSE.items()}
+def decode_word(morse_letter):
+    return REVERSE_MORSE[morse_letter]
+
+
+def decode(morse_code):
+    words = morse_code.split("|")
+    decoded_words = []
+
+    for word in words:
+        letters = word.split()
+        decoded_letters = []
+
+        for letter in letters:
+            decoded_letters.append(decode_word(letter))
+
+        decoded_words.append("".join(decoded_letters))
+
+    return " ".join(decoded_words)
 
 
 if __name__ == "__main__":
-    # Example usage for one word
-    pass  # YOUR CODE HERE
+    text = "HELLO WORLD"
+    from morse.encoder import encode
 
-    # Example usage for one sentence
-    pass  # YOUR CODE HERE
+    morse = encode(text)
+    print("Morse:", morse)
+    print("Decoded:", decode(morse))
